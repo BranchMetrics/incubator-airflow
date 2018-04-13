55b06b1ac1cc5df7c5ffda5e62f5af8a2ae1347d
aditiverma <averma@branch.io>
2018-04-13 16:34:01 -0700

    airflow/hooks/S3_hook.py

--- airflow/hooks/S3_hook.py
+++ airflow/hooks/S3_hook.py
@@ -82,8 +82,8 @@ class S3Hook(AwsHook):
         :param delimiter: the delimiter marks key hierarchy.
         :type delimiter: str
         """
-        response = self.get_conn().list_objects_v2(Bucket=bucket_name,
-                                                   Prefix=prefix,
+        response = self.get_conn().list_objects_v2(Bucket=bucket_name, 
+                                                   Prefix=prefix, 
                                                    Delimiter=delimiter)
         return [p['Prefix'] for p in response['CommonPrefixes']] if response.get('CommonPrefixes') else None
 
@@ -214,9 +214,9 @@ class S3Hook(AwsHook):
         client = self.get_conn()
         client.upload_file(filename, bucket_name, key, ExtraArgs=extra_args)
 
-    def load_string(self,
+    def load_string(self, 
                     string_data,
-                    key,
+                    key, 
                     bucket_name=None,
                     replace=False,
                     encrypt=False,
@@ -225,7 +225,7 @@ class S3Hook(AwsHook):
         Loads a string to S3
 
         This is provided as a convenience to drop a string in S3. It uses the
-        boto infrastructure to ship a file to s3.
+        boto infrastructure to ship a file to s3. 
 
         :param string_data: string to set as content for the key.
         :type string_data: str
@@ -242,15 +242,15 @@ class S3Hook(AwsHook):
         """
         if not bucket_name:
             (bucket_name, key) = self.parse_s3_url(key)
-
+        
         if not replace and self.check_for_key(key, bucket_name):
             raise ValueError("The key {key} already exists.".format(key=key))
-
+        
         extra_args={}
         if encrypt:
             extra_args['ServerSideEncryption'] = "AES256"
-
+        
         filelike_buffer = BytesIO(string_data.encode(encoding))
-
+        
         client = self.get_conn()
         client.upload_fileobj(filelike_buffer, bucket_name, key, ExtraArgs=extra_args)
